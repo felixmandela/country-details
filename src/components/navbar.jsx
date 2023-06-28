8ff0b90e1d3722dd78f0e78bfaa7c8e4b4bcac7e
Felix Mandela <felixmandela95@gmail.com>
2023-06-28 12:58:50 +0700

    add local storage to store dark mode theme

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -4,15 +4,18 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
+let theme = localStorage.getItem("theme") || "light";
+
 export default function NavBar() {
-    const [isDarkMode, setIsDarkMode] = useState(false);
+    const [mode, setMode] = useState(theme);
     useEffect(() => {
-        if (isDarkMode) {
+        if (mode === "dark") {
             document.documentElement.classList.add("dark");
         } else {
             document.documentElement.classList.remove("dark");
         }
-    }, [isDarkMode]);
+        localStorage.setItem("theme", mode);
+    }, [mode]);
 
     return (
         <header className="light-mode-elements fixed top-0 z-10 h-20 w-full border-b-[1px] border-gray-100 shadow dark:border-gray-800 dark:bg-gray-800 dark:shadow-gray-700">
@@ -26,9 +29,9 @@ export default function NavBar() {
                 </div>
                 <div
                     className="flex cursor-pointer flex-row items-center justify-center rounded-full border border-gray-100 px-4 py-4 shadow hover:bg-gray-100 dark:border-gray-800 dark:bg-gray-800 dark:shadow-gray-950 hover:dark:bg-gray-950"
-                    onClick={() => setIsDarkMode(!isDarkMode)}
+                    onClick={() => setMode(mode === "light" ? "dark" : "light")}
                 >
-                    <FontAwesomeIcon icon={isDarkMode ? faSun : faMoon} />
+                    <FontAwesomeIcon icon={mode ? faSun : faMoon} />
                 </div>
             </nav>
         </header>
